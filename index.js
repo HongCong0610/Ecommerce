8a24a93a8ef742957f92bcb2e3071648b4c1c1a3
HongCong0610 <lehongcong0610@gmail.com>
2020-09-16 20:50:34 +0700

    add cors

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const productRoute = require('./routes/product.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 mongoose.connect(
     process.env.MONGO_URL,
